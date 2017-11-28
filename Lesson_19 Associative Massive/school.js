e716fb211f6103e6d29ced717504199972f2347c
Alexandr <BerezjukAlexandr@gmail.com>
2017-11-28 19:05:12 +0200

    Adding Home-task-Lesson19-Adpative-Massive

---
+++ Lesson_19 Associative Massive/school.js
@@ -0,0 +1,11 @@
+var School = {
+			  parallel1:{classA:{pupil1:"Ivan", pupil2:"Masha", pupil3:"Petya"},
+			             classB:{pupil1:"Kolya", pupil2:"Garik", pupil3:"Tina"}
+			  			},
+			  parallel2:{classA:{pupil1:"Oxana", pupil2:"Misha", pupil3:"Lena"},
+			             classB:{pupil1:"Olga", pupil2:"Georg", pupil3:"Tanja"}
+
+ 						}
+ 			};
+
+ console.log(School["parallel2"]["classB"]["pupil1"]);
