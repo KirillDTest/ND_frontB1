85a2d53dc3aa733cac08bfb5cb4116fe106608fa
Maks <aadasd@mail.ru>
2017-11-28 20:21:55 +0200

    maket and js

---
+++ lesson 19/main.js
@@ -0,0 +1,88 @@
+var multiassociativeArray={
+                          school:{
+                parallel1:{
+                              classA:{scholar1:"Ivan", scholar2:"Natali", scholar3:"Nikita"},
+                        
+                                classB:{scholar1:"Anton", scholar2:"Jessica", scholar3:"Igor"}},
+                              
+                  parallel2:{
+                      classA:{
+                          scholar1:"Michael", scholar2:"Vitali", scholar3:"Serg"},
+                    classB:{
+                        scholar1:"Venicca", 
+                        scholar2:"Dima", 
+                        scholar3:"Alex"}
+
+
+                                }
+                                 }
+}
+                          
+
+
+console.log(multiassociativeArray["school"]["parallel2"]["classA"]["scholar2"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
