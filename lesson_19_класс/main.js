43103e6a6ad1467a37a45597d42a3c424520616d
Your Name <you@example.com>
2017-12-03 21:30:42 +0200

    ассоциативный массив про школу

---
+++ lesson_19_класс/main.js
@@ -0,0 +1,13 @@
+var School = {
+ 		  	side1:{
+ 		  			classA:{pupil1:"Evgen", pupil2:"Andrew", pupil3:"Vitaliy"},
+ 			        classB:{pupil1:"Ihor", pupil2:"Sasha", pupil3:"Inga"}
+ 			  			},
+ 		  	side2:{
+ 		  			classA:{pupil1:"Masha", pupil2:"Dima", pupil3:"Tania"},
+ 			        classB:{pupil1:"Richard", pupil2:"Grysha", pupil3:"Vasyl"}
+ 
+  						}
+ 			};
+ console.log(School["side1"]["classA"]["pupil2"]); 
+ console.log(School["side2"]["classB"]["pupil1"]); 
