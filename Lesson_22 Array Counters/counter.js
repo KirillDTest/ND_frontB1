3b678e291016f00d1e4bb7a00f692d6c403e03f5
Alexandr <BerezjukAlexandr@gmail.com>
2017-12-06 13:11:48 +0200

    Array counters

--- Lesson_22 Array Counters/counter.js
+++ Lesson_22 Array Counters/counter.js
@@ -4,25 +4,25 @@ var forest = [1,2,3,2,3,1,1,1,1,3];
 var trees=[1,2,3];
 
 
-
 var f_counter = function(arr,m) {
 
-var counter =[0,0,0];
-for (var i=0, j=arr.length; i<j; i++)
+var counter =[];                             //Обнуляем все счетчики в массиве
+for (var c=0, cm=m.length; c<cm; c++) {
+	counter[c]=0;
+}
+
+var i=0;                      //Начальное значение
+while (i<=arr.length)
 	{
-		for (var l=0, d=m.length; l<d; l++)
+	for (var l=0, d=m.length; l<d; l++)
 			{
 			  if (m[l]==arr[i])
-				 counter[l]=counter[l]+1;
+				 counter[l]++;
 			}
+	i++;                      //Приращение
 	}
 	return counter;
 }
 
 console.log(f_counter(forest,trees));
-
-
-
-
-
 
