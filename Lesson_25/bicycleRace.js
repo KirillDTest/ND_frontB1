12d3d2eadabefd1047b786d37c782d5ad742d3d2
Dimka <eheieiei37@gmail.com>
2017-12-13 19:08:26 +0300

    domash

--- Lesson_25/bicycleRace.js
+++ Lesson_25/bicycleRace.js
@@ -4,10 +4,4 @@ var bicycleRace = (s,v1,v2) => {
     }
 }
 console.log(bicycleRace(10,1,1));
-
-var bicycleRace2 = (s,v1,v2) => {
-    if(bicycleRace2) {
-        return s/(v1 + v2);
-    }
-}
-console.log(bicycleRace2(20,1,2));
+console.log(bicycleRace(20,1,2));
--- Lesson_25/triangles.js
+++ Lesson_25/triangles.js
@@ -7,13 +7,4 @@ var triangles = (a,b,c) => {
     }
 }
 console.log(triangles(3,4,5));
-
-var triangles2 = (a,b,c) => {
-    if ((a*a) + (b*b) == (c*c)) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-console.log(triangles2(1,2,4));
+console.log(triangles(1,2,4));
