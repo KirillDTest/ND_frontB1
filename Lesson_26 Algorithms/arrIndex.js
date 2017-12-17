43d778aff5d90317570839b0a53e373241b78eb1
Alexandr <BerezjukAlexandr@gmail.com>
2017-12-17 15:23:30 +0200

    Encryption

--- Lesson_26 Algorithms/arrIndex.js
+++ Lesson_26 Algorithms/arrIndex.js
@@ -1,22 +1,26 @@
 // http://www.codeabbey.com/index/task_view/sort-with-indexes--ru
+
+//////////////////////////////////////////////////////////////////////
+//////////////////FIRST VARIANT //////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 var f_sort_indexes = function(arr) {
 	var arrInd = [];
-	for (var i=0, j=arr.length; i<j; i++) {
-		arrInd[i]=[arr[i],i+1];
-		console.log(arrInd[i]);
+	for (var i=0, j=arr.length; i<j; i++) {    //Создаем многомерный массив, каждый элемент которого состоит
+		arrInd[i]=[arr[i],i+1];				   //из 1-го элемента вх.массива arr и его индекса
+		//console.log(arrInd[i]);
 	}
 	// console.log(arrInd);
 	var counter;
 	var temp;
 	do {
 		counter=0;
-		for (var i=0, j=arr.length; i<j; i++) {
-			if (arr[i]>arr[i+1]) {
+		for (var i=0, j=arr.length; i<j; i++) {  //сортируем массив с пом. метода bubbleSort
+			if (arr[i]>arr[i+1]) {               //и с перестановкой чисел в массиве arr
 				temp=arr[i+1];
 				arr[i+1]=arr[i];
 				arr[i]=temp;
 
-				temp=arrInd[i+1];
+				temp=arrInd[i+1];                //переставляем числа и в многомерном массиве
 				arrInd[i+1]=arrInd[i];
 				arrInd[i]=temp;
 
@@ -26,11 +30,58 @@ var f_sort_indexes = function(arr) {
 	}
 	while (counter>0)
 	var arrRes=[];
-		for (var i=0, j=arr.length; i<j; i++) {
+		for (var i=0, j=arr.length; i<j; i++) {   //извлекаем в новый массив индексы из отсортированного многомерного массива  
 			arrRes[i]=arrInd[i][1];
 		}
 
 	return arrRes;
 }
+console.log("First variant");
+console.log(f_sort_indexes([50, 98, 17, 79]));
 
+
+
+//////////////////////////////////////////////////////////////////////
+//////////////////SECOND VARIANT (KIRILL)/////////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+var f_bubble_sort = function(arr) {
+	do {counter=0;
+		for (var i=0, j=arr.length; i<j; i++) {
+			if (arr[i]>arr[i+1]) {
+				temp=arr[i+1];
+				arr[i+1]=arr[i];
+				arr[i]=temp;
+				counter++;
+			}
+		}
+	}
+	while (counter>0)
+		return arr;
+}
+
+var f_sort_indexes = function(arr) {
+	var counter;
+	var temp;
+	var arrRes=[];                                 //копируем arr в arrRes, 
+	for (var i=0, j=arr.length; i<j; i++) {        //чтобы при bubbleSort массив не отсортировался одновременно с arrRes (почему, см.что такое ссылка)
+		arrRes[i]=arr[i];
+	}
+
+	arrRes=f_bubble_sort(arrRes);                  //сортируем arrRes
+
+	var index;
+	var arrIndex=[];
+	for (var i=0, j=arrRes.length; i<j; i++) {      //на каждой имитерации этого цикла берем i-e значение отсортированного массива arrRes
+		index=1;
+		for (var l=0, d=arr.length; l<d; l++) {     //на каждой имитерации этого цикла берем l-e значение  неотсортированного входного массива arr и сравниваем его с arrRes[i]
+			if (arrRes[i]==arr[l])                  //если совпадение есть, то записываем номер индекса в новый массив (этот индекс это номер позиции arr[l])
+				{arrIndex[i]=index;
+				break;}
+			index++;                                //если совпадений нет, то увеличиваем индекс в конце цикла
+		}
+	}
+	return arrIndex;                //возвращаем массив индексов
+}
+console.log("Second variant");
 console.log(f_sort_indexes([50, 98, 17, 79]));
--- Lesson_26 Algorithms/masBigNum.js
+++ Lesson_26 Algorithms/masBigNum.js
@@ -6,13 +6,14 @@ var f_maximum = function(arr) {
 	for (var i=0, j=arr.length; i<j; i++) {
 		currentMax=arr[i];
 			
-		do { if (currentMax>=arr[k])
-				{k++;
-				 if (k==arr.length)
-				 	return currentMax;
+		do { if (currentMax>=arr[k])    //на каждой итерации цикла проверяем не будет ли currentMax больше следующих в массиве
+				{k++;                   //если да то k++
+				 if (k==arr.length)     //достигли конца arr?
+				 	return currentMax;  //если да то число currentMax является максимумо в массиве
 				}
-			 else {break;}
-		}
+			 else {break;}              //если currentMax оказалось меньше следующего обрываем цикл do              
+		}								//и записываем новое значение currentMax=arr[i];
+										//при этом индекс к в начале do не обнуляем, т.к. нету смысла снова сравнивать с начала массива arr, мы начинаем сравнивать currentMax с того места arr[i] где нашли новое максимальное число в массиве 
 		while (k<arr.length)
 	}
 }
