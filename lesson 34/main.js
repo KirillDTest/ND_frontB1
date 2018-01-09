fe15682ece79b823ef3fc39839736e72753ed367
Maks <aadasd@mail.ru>
2018-01-09 22:04:51 +0200

    slider

--- lesson 34/main.js
+++ lesson 34/main.js
@@ -15,3 +15,20 @@ Myfilter.addEventListener('click', function(event){
     Myfilter.style.display='none'
     Mycontainer.style.display='none'
 })
+
+
+Mycontainer.addEventListener('mousedown', function(e){
+    document.addEventListener('mousemove', MyFunc)
+});
+    function MyFunc(event)
+{     
+    Mycontainer.style.position='absolute';
+    Mycontainer.style.top= event.pageY +'px';
+    Mycontainer.style.left= event.pageX+'px';    
+          }
+
+Mycontainer.addEventListener('mouseup', function(e){
+    document.removeEventListener('mousemove', MyFunc)
+});
+
+
